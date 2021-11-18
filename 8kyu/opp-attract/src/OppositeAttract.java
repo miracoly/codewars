d39b4412c70149cb7b2edb6cc18769cac7e98628
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-11-18 14:50:17 +0100

    reorganise file structure of katas

--- 8kyu/opp-attract/src/OppositeAttract.java
+++
@@ -1,9 +0,0 @@
-public class OppositeAttract {
-
-    public static boolean isLove(final int flower1, final int flower2) {
-        boolean flower1IsEven = flower1 % 2 == 0;
-        boolean flower2IsEven = flower2 % 2 == 0;
-        return flower1IsEven != flower2IsEven;
-    }
-
-}
