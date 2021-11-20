36d0710ff3c7e74837a8706f3639119c7dc3b3c2
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-11-20 12:14:30 +0100

    add more 8th kyu katas

--- Haskell/katas/app/Codewars/Kyu8.hs
+++ Haskell/katas/app/Codewars/Kyu8.hs
@@ -2,3 +2,13 @@ module Codewars.Kyu8 where
 
 makeNegative :: (Num a, Ord a) => a -> a
 makeNegative = negate . abs
+
+boolToWord :: Bool -> String
+boolToWord True = "Yes"
+boolToWord _ = "No"
+
+repeatStr :: Int -> String -> String
+repeatStr n str = concat $ replicate n str
+
+numberToString :: Int -> String
+numberToString = show
--- Haskell/katas/test/kyu8-spec.hs
+++ Haskell/katas/test/kyu8-spec.hs
@@ -16,3 +16,24 @@ main = hspec $ do
     it "do not change negativ number" $ do
       makeNegative (-9) `shouldBe` (-9)
       makeNegative (-921) `shouldBe` (-921)
+
+  describe "Test boolToWord" $ do
+    it "returns Yes" $ do
+      boolToWord True `shouldBe` "Yes"
+    it "returns No" $ do
+      boolToWord False `shouldBe` "No"
+
+  describe "Test repeatStr" $ do
+    it "returns YesYesYes" $ do
+      repeatStr 3 "Yes" `shouldBe` "YesYesYes"
+    it "returns No" $ do
+      repeatStr 1 "No" `shouldBe` "No"
+    it "returns empty String" $ do
+      repeatStr 0 "No" `shouldBe` ""
+    it "returns empty String" $ do
+      repeatStr 0 "" `shouldBe` ""
+
+  describe "Test numberToString" $ do
+    it "return correct string" $ do
+      numberToString 123 `shouldBe` "123"
+      numberToString 999 `shouldBe` "999"
