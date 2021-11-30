8fc518c29a03ede90a1ccc642ec8499e5e8cc158
Miracoly <68049792+miracoly@users.noreply.github.com>
2021-12-01 00:11:54 +0100

    add parseMessage to CIS194 Week2

--- Haskell/katas/app/CIS194/Log/LogAnalysis.hs
+++ Haskell/katas/app/CIS194/Log/LogAnalysis.hs
@@ -3,5 +3,9 @@ module CIS194.Log.LogAnalysis where
 import CIS194.Log.Log
 
 parseMessage :: String -> LogMessage
-parseMessage = error "Deine Mudda"
-
+parseMessage m = let wl = words m in
+  case wl of
+    ("I":ts:mx) -> LogMessage Info (read ts) (unwords mx)
+    ("W":ts:mx) -> LogMessage Warning (read ts) (unwords mx)
+    ("E":sev:ts:mx) -> LogMessage (Error (read sev)) (read ts) (unwords mx)
+    _ -> Unknown $ unwords wl
